d3f490bdfb8925f7d59187ea73360cb6eb154be3
Nirmal Jayadharan <Nirmal_Jayadharan@epam.com>
2019-07-16 16:06:57 +0530

    Required Changes made

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ function loadfun() {
     mainfunction("All");
     footfunction();
 }
+
 function headfunction() {
     let x = document.getElementById("myHeader");
-    let p = create("b","News","News");
+    let p = create("b", "News", "News");
     let t = create_text("NEWSFEED");
-    let i = create("i","yet","yet");
+    let i = create("i", "yet", "yet");
     let i_text = create_text("Yet Another NewsFeed");
     i.appendChild(i_text);
     p.appendChild(t);
@@ -16,24 +17,19 @@ function headfunction() {
     document.body.appendChild(x);
 }
 
-
-
 function mainfunction(name) {
-
     let selectedOption = 0;
     let arr = ["All", "The Times Of India", "Hindustan Times", "The Hindu", "The Telegraph", "Deccan Chronicle", "Mumbai Mirror", "The Economic Times", "DNA", "The Tribune", "The New Indian Express"];
     let myMain = document.getElementById("myMain");
     myMain.innerHTML = '';
     for (let i = 1; i <= 10; i++) {
-
-        let main = create("div","main","main");
-        let center = create("div","center","center");
-        let section = create("div","section","section");
-        let section1 = create("div","section1","section1");
-        let section2 = create("div","section2","section2");
-        
+        let main = create("div", "main", "main");
+        let center = create("div", "center", "center");
+        let section = create("div", "section", "section");
+        let section1 = create("div", "section1", "section1");
+        let section2 = create("div", "section2", "section2");
         if (name == "All" || name == arr[i]) {
-            let img = create("img","img","img");
+            let img = create("img", "img", "img");
             img.setAttribute("src", "White-Blank-Wallpaper-14.png");
             section1.appendChild(img);
             section.appendChild(section1);
@@ -41,8 +37,7 @@ function mainfunction(name) {
             main.appendChild(center);
             myMain.appendChild(main);
             document.body.appendChild(myMain);
-
-            let h1 = create("h1","one","one");
+            let h1 = create("h1", "one", "one");
             let h1_t = create_text(arr[i]);
             h1.appendChild(h1_t);
             section2.appendChild(h1);
@@ -51,15 +46,13 @@ function mainfunction(name) {
             main.appendChild(center);
             myMain.appendChild(main);
             document.body.appendChild(myMain);
-
-            let p = create("p","","");
+            let p = create("p", "", "");
             spanopac("Posted On", main, center, section, section2, p);
             span(" 29 June,2019 ", main, center, section, section2, p);
             spanopac("// Category:", main, center, section, section2, p);
             span(" Category One ", main, center, section, section2, p);
-
-            p = create("p","","");
-            let p_t =create_text("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptates, nulla porro accusamus aperiam incidunt delectus quisquam totam unde, velit dolore minima. At doloremque ea eius iste fugiat dolorem sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus perspiciatis nesciunt vero mollitia voluptatum beatae, neque molestias, doloremque harum at impedit nobis optio! Quisquam corporis officia fugiat aliquid tenetur?");
+            p = create("p", "", "");
+            let p_t = create_text("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptates, nulla porro accusamus aperiam incidunt delectus quisquam totam unde, velit dolore minima. At doloremque ea eius iste fugiat dolorem sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus perspiciatis nesciunt vero mollitia voluptatum beatae, neque molestias, doloremque harum at impedit nobis optio! Quisquam corporis officia fugiat aliquid tenetur?");
             p.setAttribute("id", "text");
             p.appendChild(p_t);
             section2.appendChild(p);
@@ -68,9 +61,7 @@ function mainfunction(name) {
             main.appendChild(center);
             myMain.appendChild(main);
             document.body.appendChild(myMain);
-
-
-            let button = create("button","button",arr[i]);
+            let button = create("button", "button", arr[i]);
             let button_t = create_text("Continue Reading");
             button.appendChild(button_t);
             section2.appendChild(button);
@@ -80,32 +71,28 @@ function mainfunction(name) {
             myMain.appendChild(main);
             document.body.appendChild(myMain);
             button.setAttribute("onclick", "popupfun(id)");
-
-            let border = create("div","border","border");
+            let border = create("div", "border", "border");
             section.appendChild(border);
             center.appendChild(section);
             main.appendChild(center);
-
             myMain.appendChild(main);
             document.body.appendChild(myMain);
         }
     }
-    let side = create("div","side","side");
-    let h4 = create("h4","","");
+    let side = create("div", "side", "side");
+    let h4 = create("h4", "", "");
     let h4_t = create_text("SELECT CATEGORY");
     h4.appendChild(h4_t);
     main.appendChild(h4);
     myMain.appendChild(main);
     document.body.appendChild(myMain);
-
-
-    let drop = create("select","drop","select");
+    let drop = create("select", "drop", "select");
     let option;
     for (let i = 0; i <= 10; i++) {
-        option = create("option","","");
+        option = create("option", "", "");
         option.setAttribute("value", arr[i]);
         drop.setAttribute("onchange", "mainfunction(value)");
-        let option_t =create_text(arr[i]);
+        let option_t = create_text(arr[i]);
         option.appendChild(option_t);
         drop.appendChild(option);
         main.appendChild(drop);
@@ -116,22 +103,19 @@ function mainfunction(name) {
         }
         document.getElementById("select").selectedIndex = selectedOption;
     }
-
-    h4 = create("h4","","");
+    h4 = create("h4", "", "");
     let h4_t = create_text("SUBSCRIBE");
     h4.appendChild(h4_t);
     main.appendChild(h4);
     myMain.appendChild(main);
     document.body.appendChild(myMain);
-
-    let input = create("input","email","email");;
+    let input = create("input", "email", "email");;
     input.setAttribute("type", "text");
     input.setAttribute("placeholder", "  Email Address");
     main.appendChild(input);
     myMain.appendChild(main);
     document.body.appendChild(myMain);
-
-    input =create("input","subscribe","subscribe");
+    input = create("input", "subscribe", "subscribe");
     input.setAttribute("type", "submit");
     input.setAttribute("value", "Subscribe");
     input.addEventListener("click", function () {
@@ -144,7 +128,7 @@ function mainfunction(name) {
 
 function footfunction() {
     let foot = document.getElementById("myFooter");
-    let p = create("p","footer","");
+    let p = create("p", "footer", "");
     p.setAttribute("class", "footer");
     let p_t = create_text("NewsFeed 2019");
     p.appendChild(p_t);
@@ -158,22 +142,20 @@ function popupfun(id) {
     let para = ["All", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptates, nulla porro accusamus aperiam incidunt delectus quisquam totam unde, velit dolore minima. At doloremque ea eius iste fugiat dolorem sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus perspiciatis nesciunt vero mollitia voluptatum beatae, neque molestias, doloremque harum at impedit nobis optio! Quisquam corporis officia fugiat aliquid tenetur?", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptates, nulla porro accusamus aperiam incidunt delectus quisquam totam unde, velit dolore minima. At doloremque ea eius iste fugiat dolorem sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus perspiciatis nesciunt vero mollitia voluptatum beatae, neque molestias, doloremque harum at impedit nobis optio! Quisquam corporis officia fugiat aliquid tenetur?", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptates, nulla porro accusamus aperiam incidunt delectus quisquam totam unde, velit dolore minima. At doloremque ea eius iste fugiat dolorem sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus perspiciatis nesciunt vero mollitia voluptatum beatae, neque molestias, doloremque harum at impedit nobis optio! Quisquam corporis officia fugiat aliquid tenetur?", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptates, nulla porro accusamus aperiam incidunt delectus quisquam totam unde, velit dolore minima. At doloremque ea eius iste fugiat dolorem sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus perspiciatis nesciunt vero mollitia voluptatum beatae, neque molestias, doloremque harum at impedit nobis optio! Quisquam corporis officia fugiat aliquid tenetur?", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptates, nulla porro accusamus aperiam incidunt delectus quisquam totam unde, velit dolore minima. At doloremque ea eius iste fugiat dolorem sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus perspiciatis nesciunt vero mollitia voluptatum beatae, neque molestias, doloremque harum at impedit nobis optio! Quisquam corporis officia fugiat aliquid tenetur?", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptates, nulla porro accusamus aperiam incidunt delectus quisquam totam unde, velit dolore minima. At doloremque ea eius iste fugiat dolorem sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus perspiciatis nesciunt vero mollitia voluptatum beatae, neque molestias, doloremque harum at impedit nobis optio! Quisquam corporis officia fugiat aliquid tenetur?", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptates, nulla porro accusamus aperiam incidunt delectus quisquam totam unde, velit dolore minima. At doloremque ea eius iste fugiat dolorem sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus perspiciatis nesciunt vero mollitia voluptatum beatae, neque molestias, doloremque harum at impedit nobis optio! Quisquam corporis officia fugiat aliquid tenetur?", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptates, nulla porro accusamus aperiam incidunt delectus quisquam totam unde, velit dolore minima. At doloremque ea eius iste fugiat dolorem sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus perspiciatis nesciunt vero mollitia voluptatum beatae, neque molestias, doloremque harum at impedit nobis optio! Quisquam corporis officia fugiat aliquid tenetur?", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptates, nulla porro accusamus aperiam incidunt delectus quisquam totam unde, velit dolore minima. At doloremque ea eius iste fugiat dolorem sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus perspiciatis nesciunt vero mollitia voluptatum beatae, neque molestias, doloremque harum at impedit nobis optio! Quisquam corporis officia fugiat aliquid tenetur?", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptates, nulla porro accusamus aperiam incidunt delectus quisquam totam unde, velit dolore minima. At doloremque ea eius iste fugiat dolorem sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus perspiciatis nesciunt vero mollitia voluptatum beatae, neque molestias, doloremque harum at impedit nobis optio! Quisquam corporis officia fugiat aliquid tenetur?"]
     let arr = ["All", "The Times Of India", "Hindustan Times", "The Hindu", "The Telegraph", "Deccan Chronicle", "Mumbai Mirror", "The Economic Times", "DNA", "The Tribune", "The New Indian Express"];
     let para1;
-
     for (let j = 1; j <= 10; j++) {
         if (arr[j] == id) {
             para1 = para[j];
         }
     }
-
-    modal = create("div","modal","modal");
-    let modal_content = create("div","modal_content","");
-    let modal_head = create("div","modal_head","");
-    let close = create("span","close","");
+    modal = create("div", "modal", "modal");
+    let modal_content = create("div", "modal_content", "");
+    let modal_head = create("div", "modal_head", "");
+    let close = create("span", "close", "");
     close.setAttribute("onclick", "closefun()");
     let close_t = create_text("\u00D7");
     close.appendChild(close_t);
 
-    let modal_body = create("div","modal_body","");
+    let modal_body = create("div", "modal_body", "");
     let modal_head_text = create_text(id);
     let modal_body_text = create_text(para1);
     modal_head.appendChild(close);
@@ -190,9 +172,10 @@ function closefun() {
     let modal = document.getElementById("modal");
     modal.parentNode.removeChild(modal);
 }
-var count = 0;
-function validateEmail(email) {
 
+var count = 0;
+
+function validateEmail(email) {
     let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (mailformat.test(email)) {
 
@@ -207,8 +190,7 @@ function validateEmail(email) {
 }
 
 function spanopac(content, main, center, section, section2, p) {
-
-    let span1 = create("span","opac","");
+    let span1 = create("span", "opac", "");
     let span1_t = create_text(content);
     span1.appendChild(span1_t);
     p.appendChild(span1);
@@ -218,12 +200,11 @@ function spanopac(content, main, center, section, section2, p) {
     main.appendChild(center);
     myMain.appendChild(main);
     document.body.appendChild(myMain);
-
 }
-function span(content, main, center, section, section2, p) {
 
-    let span1 =create("span","","");
-    let span1_t =create_text(content);
+function span(content, main, center, section, section2, p) {
+    let span1 = create("span", "", "");
+    let span1_t = create_text(content);
     span1.appendChild(span1_t);
     p.appendChild(span1);
     section2.appendChild(p);
@@ -232,18 +213,17 @@ function span(content, main, center, section, section2, p) {
     main.appendChild(center);
     myMain.appendChild(main);
     document.body.appendChild(myMain);
-
 }
 
-function create(element,class_name,id_name){
-    let main=document.createElement(element);
+function create(element, class_name, id_name) {
+    let main = document.createElement(element);
     main.setAttribute("class", class_name);
     main.setAttribute("id", id_name);
-    return(main);
+    return (main);
 }
 
-function create_text(element_text){
-  let text =document.createTextNode(element_text);
-  return(text);
-}   
+function create_text(element_text) {
+    let text = document.createTextNode(element_text);
+    return (text);
+}
 
