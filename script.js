da106b932aa542b82f7098a3337d88ebcc8c8e43
Nirmal Jayadharan <Nirmal_Jayadharan@epam.com>
2019-07-23 17:51:37 +0530

    Required Changes made

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const arr = ["All", "The Times Of India", "Hindustan Times", "The Hindu", "The Telegraph", "Deccan Chronicle", "Mumbai Mirror", "The Economic Times", "DNA", "The Tribune", "The New Indian Express"];
+let myMain = document.getElementById("myMain");
+let main = create_element("div", "main", "main");
+let center = create_element("div", "center", "center");
+let side = create_element("div", "side", "side");
+
 function load() {
     header_section();
-    body_section("All");
+    body_section();
     footer_section();
 }
 
@@ -17,35 +23,17 @@ function header_section() {
     document.body.appendChild(x);
 }
 
-function body_section(name) {
-    const arr = ["All", "The Times Of India", "Hindustan Times", "The Hindu", "The Telegraph", "Deccan Chronicle", "Mumbai Mirror", "The Economic Times", "DNA", "The Tribune", "The New Indian Express"];
-    let myMain = document.getElementById("myMain");
-    myMain.innerHTML = '';
+function body_section() {
+    center.innerHTML = '';
+    side.innerHTML = '';
     for (let i = 1; i <= 10; i++) {
-        var main = create_element("div", "main", "main");
-        let center = create_element("div", "center", "center");
         let section = create_element("div", "section", "section");
         let section1 = create_element("div", "section1", "section1");
         let section2 = create_element("div", "section2", "section2");
-        if (name == "All" || name == arr[i]) {
-
-            create_img(section1, section, center, main, myMain);
-            create_title(section2, section, center, main, myMain, arr[i]);
-            let p = create_element("p", "", "");
-            spanopac("Posted On", main, center, section, section2, p);
-            span(" 29 June,2019 ", main, center, section, section2, p);
-            spanopac("// Category:", main, center, section, section2, p);
-            span(" Category One ", main, center, section, section2, p);
-            p = create_element("p", "", "");
-            create_content(section2, section, center, main, myMain, p);
-            create_button(section2, section, center, main, myMain, arr[i]);
-            create_border(section, center, main, myMain);
-
-
-        }
+        create_tile(arr[i]);
     }
-    create_dropdown(main, myMain, arr, name);
-    create_email(main, myMain);
+    create_dropdown(name);
+    create_email();
 }
 
 function footer_section() {
@@ -108,30 +96,23 @@ function validate_Email(email) {
     }
 }
 
-function spanopac(content, main, center, section, section2, p) {
+function span_opacity(content, section, section2, p) {
     let span1 = create_element("span", "opac", "");
     let span1_t = create_text(content);
     span1.appendChild(span1_t);
     p.appendChild(span1);
     section2.appendChild(p);
     section.appendChild(section2);
-    center.appendChild(section);
-    main.appendChild(center);
-    myMain.appendChild(main);
-    document.body.appendChild(myMain);
+    append_child_center(section);
 }
 
-function span(content, main, center, section, section2, p) {
+function span(content, section, section2, p) {
     let span1 = create_element("span", "", "");
     let span1_t = create_text(content);
     span1.appendChild(span1_t);
     p.appendChild(span1);
     section2.appendChild(p);
-    section.appendChild(section2);
-    center.appendChild(section);
-    main.appendChild(center);
-    myMain.appendChild(main);
-    document.body.appendChild(myMain);
+    append_child_center(section);
 }
 
 function create_element(element, class_name, id_name) {
@@ -146,113 +127,126 @@ function create_text(element_text) {
     return (text);
 }
 
-function create_img(section1, section, center, main, myMain) {
+function create_img(section1, section) {
     let img = create_element("img", "img", "img");
     img.setAttribute("src", "White-Blank-Wallpaper-14.png");
     section1.appendChild(img);
     section.appendChild(section1);
-    center.appendChild(section);
-    main.appendChild(center);
-    myMain.appendChild(main);
-    document.body.appendChild(myMain);
+    append_child_center(section);
 }
 
-function create_title(section2, section, center, main, myMain, paper_name) {
+function create_title(section2, section, paper_name) {
     let h1 = create_element("h1", "one", "one");
     let h1_t = create_text(paper_name);
     h1.appendChild(h1_t);
     section2.appendChild(h1);
     section.appendChild(section2);
-    center.appendChild(section);
-    main.appendChild(center);
-    myMain.appendChild(main);
-    document.body.appendChild(myMain);
-
+    append_child_center(section);
 }
 
-function create_content(section2, section, center, main, myMain, p) {
+function create_content(section2, section, p) {
     let p_t = create_text("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi voluptates, nulla porro accusamus aperiam incidunt delectus quisquam totam unde, velit dolore minima. At doloremque ea eius iste fugiat dolorem sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ducimus perspiciatis nesciunt vero mollitia voluptatum beatae, neque molestias, doloremque harum at impedit nobis optio! Quisquam corporis officia fugiat aliquid tenetur?");
     p.setAttribute("id", "text");
     p.appendChild(p_t);
     section2.appendChild(p);
     section.appendChild(section2);
-    center.appendChild(section);
-    main.appendChild(center);
-    myMain.appendChild(main);
-    document.body.appendChild(myMain);
-
+    append_child_center(section);
 }
 
-function create_button(section2, section, center, main, myMain, paper_name) {
+function create_button(section2, section, paper_name) {
     let button = create_element("button", "button", paper_name);
     let button_t = create_text("Continue Reading");
     button.appendChild(button_t);
     section2.appendChild(button);
     section.appendChild(section2);
-    center.appendChild(section);
-    main.appendChild(center);
-    myMain.appendChild(main);
-    document.body.appendChild(myMain);
+    append_child_center(section);
     button.setAttribute("onclick", "modal_window(id)");
 }
 
-function create_border(section, center, main, myMain) {
+function create_border(section) {
     let border = create_element("div", "border", "border");
     section.appendChild(border);
-    center.appendChild(section);
-    main.appendChild(center);
-    myMain.appendChild(main);
-    document.body.appendChild(myMain);
+    append_child_center(section);
 }
 
-function create_dropdown(main, myMain, arr, name) {
+function create_dropdown(name) {
     let selectedOption = 0;
-    let side = create_element("div", "side", "side");
     let h4 = create_element("h4", "", "");
-    var h4_t = create_text("SELECT CATEGORY");
+    let h4_t = create_text("SELECT CATEGORY");
     h4.appendChild(h4_t);
-    main.appendChild(h4);
-    myMain.appendChild(main);
-    document.body.appendChild(myMain);
+    append_child_side(h4);
     let drop = create_element("select", "drop", "select");
     let option;
     for (let i = 0; i <= 10; i++) {
         option = create_element("option", "", "");
         option.setAttribute("value", arr[i]);
-        drop.setAttribute("onchange", "body_section(value)");
+        drop.setAttribute("onchange", "create_single_tile(value)");
         let option_t = create_text(arr[i]);
         option.appendChild(option_t);
         drop.appendChild(option);
-        main.appendChild(drop);
-        myMain.appendChild(main);
-        document.body.appendChild(myMain);
+        append_child_side(drop);
         if (arr[i] == name) {
             selectedOption = i;
         }
-        document.getElementById("select").selectedIndex = selectedOption;
     }
+    document.getElementById("select").selectedIndex = selectedOption;
 }
 
-function create_email(main, myMain) {
+function create_email() {
     h4 = create_element("h4", "", "");
-    var h4_t = create_text("SUBSCRIBE");
+    let h4_t = create_text("SUBSCRIBE");
     h4.appendChild(h4_t);
-    main.appendChild(h4);
-    myMain.appendChild(main);
-    document.body.appendChild(myMain);
+    append_child_side(h4);
     let input = create_element("input", "email", "email");;
     input.setAttribute("type", "text");
     input.setAttribute("placeholder", "  Email Address");
-    main.appendChild(input);
-    myMain.appendChild(main);
-    document.body.appendChild(myMain);
+    append_child_side(input);
     input = create_element("input", "subscribe", "subscribe");
     input.setAttribute("type", "submit");
     input.setAttribute("value", "Subscribe");
     input.addEventListener("click", function () {
         validate_Email(document.getElementById("email").value);
     })
-    main.appendChild(input);
+    append_child_side(input);
+}
+
+function create_single_tile(value) {
+    center.innerHTML = '';
+    if (value == "All") {
+        body_section();
+    }
+    else {
+        create_tile(value);
+    }
+}
+
+function create_tile(name) {
+    let section = create_element("div", "section", "section");
+    let section1 = create_element("div", "section1", "section1");
+    let section2 = create_element("div", "section2", "section2");
+    create_img(section1, section);
+    create_title(section2, section, name);
+    let p = create_element("p", "", "");
+    span_opacity("Posted On", section, section2, p);
+    span(" 29 June,2019 ", section, section2, p);
+    span_opacity("// Category:", section, section2, p);
+    span(" Category One ", section, section2, p);
+    p = create_element("p", "", "");
+    create_content(section2, section, p);
+    create_button(section2, section, name);
+    create_border(section);
+}
+
+function append_child_center(value) {
+    center.appendChild(value);
+    main.appendChild(center);
+    myMain.appendChild(main);
+    document.body.appendChild(myMain);
+}
+
+function append_child_side(value) {
+    side.appendChild(value);
+    main.appendChild(side);
     myMain.appendChild(main);
     document.body.appendChild(myMain);
 }
